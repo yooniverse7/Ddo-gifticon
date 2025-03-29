fcb22a290dc2c560b02887094e67a36b8eefe565
jeong young han <daum4572@naver.com>
2025-03-30 02:55:35 +0900

    Feat: pos-service 모듈 생성

---
+++ BE/pos-service/src/test/java/com/ddopay/posservice/PosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddopay.posservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
