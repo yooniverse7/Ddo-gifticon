42efd7a566c05616dcdc2496c8d5df82e3ba02cf
jeong young han <daum4572@naver.com>
2025-03-30 02:52:19 +0900

    Feat: common-config 모듈 생성

---
+++ BE/common-config/src/test/java/com/ddopay/commonconfig/CommonConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddopay.commonconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
