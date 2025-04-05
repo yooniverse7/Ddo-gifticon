f08b16dec9730b03b88db0c0605fca7b85fa639a
최진문 <wlsansdlek1@me.com>
2025-04-05 19:55:14 +0900

    Chore: splash 이미지 추가(이미지 사이즈 조절 필요)

--- FE/mobile/android/app/src/main/java/com/mobile/MainActivity.kt
+++ FE/mobile/android/app/src/main/java/com/mobile/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.mobile
 
+import android.os.Bundle // 새로 추가
+import org.devio.rn.splashscreen.SplashScreen // 새로 추가
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this) // 앱 시작 시 스플래시 화면 표시
+    super.onCreate(savedInstanceState)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
