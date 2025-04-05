486702a1af19f694745e32bb0051347dd820f006
최진문 <wlsansdlek1@me.com>
2025-04-05 22:42:38 +0900

    Chore: web 테스트를 위한 middleware 우회

--- FE/web/src/features/permissonRequest/api/handleMessageToRn.ts
+++ FE/web/src/features/permissonRequest/api/handleMessageToRn.ts
@@ -6,5 +6,10 @@ export const handleMessageToRn = () => {
         type: 'OPEN_PERMISSION_REQUEST',
       })
     );
+  } else {
+    // Web 환경: 접근 권한 쿠키를 임의로 설정하여 미들웨어를 우회함
+    document.cookie = 'accessPermission=true; path=/';
+    console.log('Web 환경에서 accessPermission 쿠키를 자동 설정했습니다.');
+    window.location.reload();
   }
 };
